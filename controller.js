7d2be6be1d68bb9198bd62edfb4fe2316879e6b1
Abhi005shek <abhishek005vk+vk.com>
2025-07-02 16:30:51 +0530

    small bug

--- controller.js
+++ controller.js
@@ -8,7 +8,8 @@ const https = require("https");
 // const ffmpegPath = "/usr/bin/ffmpeg";
 // const ytdlpPath = "/usr/local/bin/yt-dlp";
 // const ytdlpPath = path.join(__dirname, "bin", "yt-dlp");
-const ytdlpPath = "yt-dlp";
+// const ytdlpPath = "yt-dlp";
+const ytdlpPath = path.join(__dirname, "bin", "yt-dlp");
 const userAgent = `--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36"`
 
 function sanitizeFilename(name) {
